8fc7cc7e02340c70b5f2f188377f1ae96b0a74b2
simba77 <simba7@inbox.ru>
2024-10-25 12:09:58 +0300

    Добавлено описание работы с папкой public

--- src/main.js
+++ src/main.js
@@ -1,10 +1,37 @@
 import './scss/style.scss'
 import 'bootstrap'
 import {Popover} from 'bootstrap'
-import Jquery from 'jquery'
-import Swiper from 'swiper';
-import {Navigation, Pagination, EffectFade, Thumbs} from 'swiper/modules';
-import 'swiper/css';
+// import Jquery from 'jquery'
+// import Swiper from 'swiper';
+// import {Navigation, Pagination, EffectFade, Thumbs} from 'swiper/modules';
+// import 'swiper/css';
 
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new Popover(popoverTriggerEl))
+
+/*
+new Swiper('.example-slider', {
+  slidesPerView: 5,
+  breakpoints: {
+    200: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    400: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    550: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    992: {
+      slidesPerView: 5,
+      spaceBetween: 20
+    }
+  },
+  mousewheel: {
+    releaseOnEdges: true
+  },
+})
+*/
